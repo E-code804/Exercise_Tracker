6c48161af2e24355c0fbf0a721a0ed8efe31d122
Erik Pfeffer <epfeffer@terpmail.umd.edu>
2025-05-13 11:41:16 -0400

    Implement post and get req for /api/users.

--- index.js
+++ index.js
@@ -1,18 +1,59 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+});
 
+const User = mongoose.model("User", userSchema);
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+app.get("/api/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+
+    return res.status(200).send({ users });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Server error" });
+  }
+});
+
+app.post("/api/users", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const newUser = await User.create({
+      username,
+    });
+
+    return res.status(201).send({ username, _id: newUser._id });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Server error" });
+  }
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
